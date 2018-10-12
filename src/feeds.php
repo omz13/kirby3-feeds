efabc017ef21c56745965600008814060fd5a50e
David Somers <dsomers@omz13.com>
2018-10-12 09:34:00 +0200

    Release of new version 0.2.0

--- src/feeds.php
+++ src/feeds.php
@@ -46,7 +46,7 @@ use function strtotime;
 use function time;
 use function ucwords;
 
-define( 'FEEDS_VERSION', '0.1.0' );
+define( 'FEEDS_VERSION', '0.2.0' );
 define( 'FEEDS_CONFIGURATION_PREFIX', 'omz13.feeds' );
 
 /**
